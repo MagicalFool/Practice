728e885065993effac9bf004ae4cb78a481c742b
chenchao <chenchao@caiex.com>
2020-01-14 17:50:29 +0800

    concurrent map

--- src/main/java/practice014_concurrent/Demo.java
+++ src/main/java/practice014_concurrent/Demo.java
@@ -1,4 +1,26 @@
 package practice014_concurrent;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
 public class Demo {
+
+    public static void main(String[] args) {
+        ConcurrentHashMap hashMap = new ConcurrentHashMap();
+        HashMap map = new HashMap();
+        Hashtable<Object, Object> table = new Hashtable<Object, Object>();
+//        Object put = table.put("1", "2");
+//        table.put("1","3");
+//        Collections.synchronizedMap(map);
+//        System.out.println(put);
+        new ThreadDemo(map,0,30).start();
+        new ThreadDemo(map,30,60).start();
+        new ThreadDemo(map,60,90).start();
+
+        System.out.println(map);
+        System.out.println(map.size());
+    }
 }
---
+++ src/main/java/practice014_concurrent/ThreadDemo.java
@@ -0,0 +1,32 @@
+package practice014_concurrent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ThreadDemo extends Thread {
+
+    private Map map;
+
+    private int start;
+
+    private int end;
+
+    public ThreadDemo(Map map,int start, int end) {
+        this.map = map;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public void run() {
+        for (int i = start; i < end ; i++) {
+            map.put(i,i+"_");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
